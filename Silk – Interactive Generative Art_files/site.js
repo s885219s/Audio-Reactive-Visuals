2287c261ae9b4fdb4b2862f445c7a41dcb828e82
Arick Chen <s885219s@gmail.com>
2016-04-01 14:01:59 +0800

    micOnly

--- Silk – Interactive Generative Art_files/site.js
+++ Silk – Interactive Generative Art_files/site.js
@@ -1492,16 +1492,37 @@
         //}
         //_this.useMic=true;
         //x-browser
-        if (!navigator.mediaDevices.getUserMedia)
-          navigator.mediaDevices.getUserMedia = (navigator.mediaDevices.getUserMedia ||
-          navigator.mediaDevices.webkitGetUserMedia ||
-          navigator.mediaDevices.mozGetUserMedia ||
-          navigator.mediaDevices.msGetUserMedia);
-        if (!navigator.cancelAnimationFrame)
-          navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
-        if (!navigator.requestAnimationFrame)
-          navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
+        var promisifiedOldGUM = function(constraints, successCallback, errorCallback) {
 
+          // First get ahold of getUserMedia, if present
+          var getUserMedia = (navigator.getUserMedia ||
+          navigator.webkitGetUserMedia ||
+          navigator.mozGetUserMedia);
+
+          // Some browsers just don't implement it - return a rejected promise with an error
+          // to keep a consistent interface
+          if(!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+          }
+
+          // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+          return new Promise(function(successCallback, errorCallback) {
+            getUserMedia.call(navigator, constraints, successCallback, errorCallback);
+          });
+
+        }
+
+// Older browsers might not implement mediaDevices at all, so we set an empty object first
+        if(navigator.mediaDevices === undefined) {
+          navigator.mediaDevices = {};
+        }
+
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+        if(navigator.mediaDevices.getUserMedia === undefined) {
+          navigator.mediaDevices.getUserMedia = promisifiedOldGUM;
+        }
         navigator.mediaDevices.getUserMedia(
             {
               //也等於 {audio: true}
