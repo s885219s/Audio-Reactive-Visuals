9c6ad5afa496ae9ffa465e29968d5eb843783614
Arick Chen <s885219s@gmail.com>
2016-04-01 15:13:56 +0800

    finalPLZ

--- Silk – Interactive Generative Art_files/site.js
+++ Silk – Interactive Generative Art_files/site.js
@@ -1493,36 +1493,44 @@
         //_this.useMic=true;
         //x-browser
 
-        if (!navigator.mediaDevices.getUserMedia)
-          navigator.mediaDevices.getUserMedia = (navigator.mediaDevices.getUserMedia ||
-          navigator.mediaDevices.webkitGetUserMedia ||
-          navigator.mediaDevices.mozGetUserMedia ||
-          navigator.mediaDevices.msGetUserMedia ||
-          navigator.getUserMedia ||
+        var promisifiedOldGUM = function(constraints, successCallback, errorCallback) {
+
+          // First get ahold of getUserMedia, if present
+          var getUserMedia = (navigator.getUserMedia ||
           navigator.webkitGetUserMedia ||
-          navigator.mozGetUserMedia ||
-          navigator.msGetUserMedia);
-        
-        if (!navigator.cancelAnimationFrame)
-          navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
-        if (!navigator.requestAnimationFrame)
-          navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
+          navigator.mozGetUserMedia);
 
-        navigator.mediaDevices.getUserMedia(
-            {
-              //也等於 {audio: true}
-              "audio": {
-                "mandatory": {
-                  "googEchoCancellation": "false",
-                  "googAutoGainControl": "false",
-                  "googNoiseSuppression": "false",
-                  "googHighpassFilter": "false"
-                },
-                "optional": []
-              }
-            },
+          // Some browsers just don't implement it - return a rejected promise with an error
+          // to keep a consistent interface
+          if(!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+          }
 
-            function(stream) {
+          // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+          return new Promise(function(successCallback, errorCallback) {
+            getUserMedia.call(navigator, constraints, successCallback, errorCallback);
+          });
+
+        }
+
+        // Older browsers might not implement mediaDevices at all, so we set an empty object first
+        if(navigator.mediaDevices === undefined) {
+          navigator.mediaDevices = {};
+        }
+
+        // Some browsers partially implement mediaDevices. We can't just assign an object
+        // with getUserMedia as it would overwrite existing properties.
+        // Here, we will just add the getUserMedia property if it's missing.
+        if(navigator.mediaDevices.getUserMedia === undefined) {
+          navigator.mediaDevices.getUserMedia = promisifiedOldGUM;
+        }
+
+
+        // Prefer camera resolution nearest to 1280x720.
+        var constraints = { audio: true };
+
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(function(stream) {
               inputPoint = audioContext.createGain();
               realAudioInput = audioContext.createMediaStreamSource(stream);
               audioInput = realAudioInput;
@@ -1537,12 +1545,61 @@
 
               //isPlayingAudio = true;
               updateAnalysers();
-            },
-            // errorCallback
-            function(err) {
-              alert("The following error occured: " + err);
-            }
-        );
+            })
+            .catch(function(err) {
+              console.log(err.name + ": " + err.message);
+            });
+
+        //if (!navigator.mediaDevices.getUserMedia)
+        //  navigator.mediaDevices.getUserMedia = (navigator.mediaDevices.getUserMedia ||
+        //  navigator.mediaDevices.webkitGetUserMedia ||
+        //  navigator.mediaDevices.mozGetUserMedia ||
+        //  navigator.mediaDevices.msGetUserMedia ||
+        //  navigator.getUserMedia ||
+        //  navigator.webkitGetUserMedia ||
+        //  navigator.mozGetUserMedia ||
+        //  navigator.msGetUserMedia);
+        //
+        //if (!navigator.cancelAnimationFrame)
+        //  navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
+        //if (!navigator.requestAnimationFrame)
+        //  navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
+        //
+        //navigator.mediaDevices.getUserMedia(
+        //    {
+        //      //也等於 {audio: true}
+        //      "audio": {
+        //        "mandatory": {
+        //          "googEchoCancellation": "false",
+        //          "googAutoGainControl": "false",
+        //          "googNoiseSuppression": "false",
+        //          "googHighpassFilter": "false"
+        //        },
+        //        "optional": []
+        //      }
+        //    },
+        //
+        //    function(stream) {
+        //      inputPoint = audioContext.createGain();
+        //      realAudioInput = audioContext.createMediaStreamSource(stream);
+        //      audioInput = realAudioInput;
+        //      audioInput.connect(inputPoint);
+        //
+        //      //reinit here or get an echo on the mic
+        //      micsoundSource = audioContext.createBufferSource();
+        //      micanalyserNode = audioContext.createAnalyser();
+        //      micanalyserNode.fftSize = 1024;
+        //      micanalyserNode.smoothingTimeConstant = 0.3;
+        //      inputPoint.connect( micanalyserNode );
+        //
+        //      //isPlayingAudio = true;
+        //      updateAnalysers();
+        //    },
+        //    // errorCallback
+        //    function(err) {
+        //      alert("The following error occured: " + err);
+        //    }
+        //);
       }
       var MIN_SAMPLES = 0;
       function autoCorrelate( buf, sampleRate ) {
@@ -1610,8 +1667,8 @@
         var micmultiplier = micanalyserNode.frequencyBinCount;
         //console.log(micfreqByteData);
 
-        var timeByteData = new Uint8Array(analyserNode.frequencyBinCount);
-        analyserNode.getFloatTimeDomainData(timeByteData);
+        //var timeByteData = new Uint8Array(analyserNode.frequencyBinCount);
+        //analyserNode.getFloatTimeDomainData(timeByteData);
 
         //analyserNode.getFloatTimeDomainData( buf );
 
